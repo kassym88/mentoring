4038f6dd75e00b764c2098684679ef9318c3ee72
kassym88 <kassym88@gmail.com>
2018-11-30 17:37:40 +0600

    First

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { BreadcrumbsComponent } from './components/breadcrumbs/breadcrumbs.component';
+import { CourseitemComponent } from './components/courseitem/courseitem.component';
+import { CourselistComponent } from './components/courselist/courselist.component';
+import { FooterComponent } from './components/footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    BreadcrumbsComponent,
+    CourseitemComponent,
+    CourselistComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/components/courseitem/courseitem.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-courseitem',
+  templateUrl: './courseitem.component.html',
+  styleUrls: ['./courseitem.component.css']
+})
+export class CourseitemComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
