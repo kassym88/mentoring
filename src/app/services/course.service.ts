2ada70c6a90bce6663a92d5b683bba3ec1e70bb0
kassym88 <kassym88@gmail.com>
2019-01-18 17:33:39 +0600

    courseService

--- src/app/services/course.service.ts
+++ src/app/services/course.service.ts
@@ -7,10 +7,37 @@ import { Courses } from '../components/courselist/mock.courelist';
   providedIn: 'root'
 })
 export class CourseService {
-
-  constructor() { }
+  courses: Course[];
+  constructor() {
+    this.courses = Courses;
+  }
 
   getCourseList(): Observable<Course[]> {
-    return of(Courses);
+    return of(this.courses);
+  }
+
+  createCourse(newCourse: Course): Observable<Course[]> {
+    this.courses.push(newCourse);
+    return of(this.courses);
+  }
+
+  getItemById(id: number): Course {
+    return this.courses.find((course: Course) => course.id === id);
+  }
+
+  updateItem(updatedCourse: Course): Observable<Course[]> {
+    const curCourse: Course = this.courses.find((course: Course) => course.id === updatedCourse.id);
+    if (curCourse) {
+      Object.keys(curCourse).forEach((key: string) => curCourse[key] = updatedCourse[key]);
+    }
+    return of(this.courses);
+  }
+
+  removeItem(removeCourse: Course): Observable<Course[]> {
+    const x: number = this.courses.findIndex((course: Course) => course.id === removeCourse.id) || -1;
+    if (x > -1) {
+      this.courses = this.courses.splice(x, 1);
+    }
+    return of(this.courses);
   }
 }
