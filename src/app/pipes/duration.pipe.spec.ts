00b03451d268760c5552e256e7832bf774ef6b68
kassym88 <kassym88@gmail.com>
2019-04-03 17:07:25 +0600

    testing in progress

--- src/app/pipes/duration.pipe.spec.ts
+++ src/app/pipes/duration.pipe.spec.ts
@@ -5,4 +5,9 @@ describe('DurationPipe', () => {
     const pipe = new DurationPipe();
     expect(pipe).toBeTruthy();
   });
+
+  it('should return formatted time 80 => 1h 20min', () => {
+    const pipe = new DurationPipe();
+    expect(pipe.transform(80)).toEqual('1h 20min');
+  });
 });
